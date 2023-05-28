f1d5af43d71edf7ef0631b7d5e0524a87fb7ab32
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-05-28 01:29:27 -0400

    Removing legacy files :

--- include/Apollo-Lib/api.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "Apollo-Lib/drive/chassis.hpp"
-#include "Apollo-Lib/util/util.hpp"
-#include "Apollo-Lib/util/pid.hpp"
--- include/main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef _VEX_MAIN_H_
-#define _VEX_MAIN_H_
-#include "api.h"
-void initialize(void);
-void autonomous_control(void);
-void user_control(void);
-int main(void);
-#endif
