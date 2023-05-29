69784d0dd1fce6acfc7f581ce42c203739ffe6ab
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-05-29 14:34:35 -0400

    VSCode Import

--- include/apollo/api/util/util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "vex_brain.h"
-#include "vex_competition.h"
-#include "vex_controller.h"
-extern vex::brain brain_master;
-extern vex::controller master;
-extern vex::competition competition_master;
--- src/apollo/api/util/util.cpp
+++
@@ -1,4 +0,0 @@
-#include "api.h"
-vex::competition competition_master = vex::competition();
-vex::brain brain_master = vex::brain();
-vex::controller master = vex::controller(vex::controllerType::primary);
