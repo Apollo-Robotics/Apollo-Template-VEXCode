957549029bef8aeaaf134f3744b13f96b5494815
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-05-30 00:01:24 -0400

    Starting util

---
+++ include/apollo/api/util/util.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "vex_brain.h"
+#include "vex_competition.h"
+#include "vex_controller.h"
+extern vex::brain brain_master;
+extern vex::controller master;
+extern vex::competition competition_master;
+namespace apollo
+{
+    enum directionType
+    {
+        LEFT = 0,
+        RIGHT = 1
+    };
+} // namespace apollo
